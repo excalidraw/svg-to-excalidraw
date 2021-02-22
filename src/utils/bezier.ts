6f8c91f6e8496b28fcb92343540a87d97c51ee0f
Nicolas Goudry <goudry.nicolas@gmail.com>
2021-02-22 19:53:08 +0100

    feat(bezier): enhance cubic bézier curve generation
    - add docs
    - update name of function to get points
    - refactor precision argument to be the number of points to return (without origin point)
    - use Number constructor rather than implicit type coercion (+)

--- src/utils/bezier.ts
+++ src/utils/bezier.ts
@@ -1,21 +1,40 @@
-function getPointAt(controlPoints, step) {
-  return Array.from({ length: 2 }).map(
-    (v, i) =>
-      +(
-        controlPoints[0][i] * (1 - step) ** 3 +
-        3 * controlPoints[1][i] * step * (1 - step) ** 2 +
-        3 * controlPoints[2][i] * step ** 2 * (1 - step) +
-        controlPoints[3][i] * step ** 3
-      ).toFixed(2),
-  );
+/**
+ * Get coordinates x,y of a point at a given section of a cubic bezier curve
+ * @see https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Cubic_B%C3%A9zier_curves
+ */
+function getPointOfCubicCurve(
+  controlPoints: number[][],
+  section: number,
+): number[] {
+  // Support only two dimensions curves
+  return Array.from({ length: 2 }).map((v, i) => {
+    const pointCoordinate =
+      controlPoints[0][i] * (1 - section) ** 3 +
+      3 * controlPoints[1][i] * section * (1 - section) ** 2 +
+      3 * controlPoints[2][i] * section ** 2 * (1 - section) +
+      controlPoints[3][i] * section ** 3;
+
+    return Number(pointCoordinate.toFixed(2));
+  });
 }
 
-export function cubic(controlPoints, precision = 20) {
-  if (precision === 100 || 100 % precision) {
-    throw new Error("Precision must be one of 1, 2, 4, 5, 10, 20, 25, 50");
+/**
+ * Get list of coordinates for a cubic bézier curve
+ * Starting point is not returned
+ */
+export function cubicCurveToPoints(
+  controlPoints: number[][],
+  nbPoints = 4,
+): number[][] {
+  if (nbPoints <= 0) {
+    throw new Error("Precision must be positive");
+  } else if (nbPoints > 100) {
+    nbPoints = 100;
   }
 
-  return Array.from({ length: 100 / precision - 1 }).map((value, index) =>
-    getPointAt(controlPoints, ((index + 1) * precision) / 100),
-  );
+  return Array.from({ length: nbPoints }, (value, index) => {
+    const section = Number((((100 / nbPoints) * (index + 1)) / 100).toFixed(2));
+
+    return getPointOfCubicCurve(controlPoints, section);
+  });
 }
