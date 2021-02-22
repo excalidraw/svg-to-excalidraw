be3162b78011b63e3d2de6f6515fee0f983bf1ac
Nicolas Goudry <nicolas-goudry@users.noreply.github.com>
2021-02-23 00:08:14 +0100

    chore: initial project setup (#1)
    * chore: initialize npm
    
    * chore: add .gitignore
    
    * chore(deps): add husky
    
    * chore: setup husky
    
    Auto-install husky on postinstall npm lifecycle event
    
    * chore(deps): add commitlint
    
    With conventional commits config and prompt-cli
    
    * chore: setup commitlint
    
    - conventional commits config
    - auto-run on commit-msg/pre-push git hooks
    - easier access to commit-style guidelines through CLI prompt
    
    * chore(deps): add eslint
    
    With standard code style
    
    * chore: setup eslint
    
    - add config
    - add npm script
    - add pre-commit git hook
    
    * chore: add github issue templates
    
    * docs: add README, COC and Contributing guide
    
    * chore: add npm lockfile
    
    * docs: add slack link to contributing guide
    
    * docs: use excalidraw's discord for questions
    
    * chore: update eslint config
    
    * chore: add typescript and webpack
    
    Adding basic Typescript support, as well as a Webpack build pipeline.
    
    * chore: add additional ESM build
    
    So that dist build may be used directly in dev html docs
    
    * chore(deps): update dependencies
    
    * docs: add beginning of local dev docs
    
    * chore(code): add prettier
    
    Add Prettier library and run code formatting
    
    * chore(deps): use caret version range for all dependencies
    
    * docs: remove code of conduct
    
    Wait for core code of conduct
    See https://github.com/excalidraw/svg-to-excalidraw/pull/1#issuecomment-783455890
    
    * chore: remove issue templates
    
    See https://github.com/excalidraw/svg-to-excalidraw/pull/1#issuecomment-783455890
    
    * docs: update contributing guide
    
    * chore: switch to yarn
    
    * chore: use @excalidraw/prettier-config
    
    * chore: add and setup lint-staged
    
    * style: missing eol
    
    * chore(scripts): format eslint scripts name
    
    Match prettier scripts name format
    
    * chore: remove pre-push hook
    
    * style: run prettier
    
    * chore: update gitignore
    
    * ci: setup base actions workflows
    
    * docs: update readme
    
    - remove standard badge
    - move local dev section
    - add local dev section to TOC
    
    * chore(webpack): move webpack cli to dev dependency
    
    * chore(deps): remove standardjs eslint config
    
    * docs(githooks): add note about using hooks with Git GUIs
    
    * chore: missing EOL
    
    * docs: use yarn commands in instructions
    
    * chore(scripts): use yarn instead of npm
    
    * style: fix dependabot config
    
    * ci: fix commitlint step by using predefined action
    
    * ci: remove commitlint from lint workflow
    
    * chore(scripts): update all scripts
    
    - add fix/test scripts
    - remove prettier scripts (replaced by fix scripts)
    - remove commit script
    - order scripts alphabetically
    
    * chore: remove commitlint entirely
    
    * ci: fix lint workflow
    
    * chore(scripts): add typecheck to tests
    
    * chore(deps): use fixed package versions
    
    * chore(webpack): remove commented code
    
    Co-authored-by: Broch Stilley <brochington@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+
+const aliases = {};
+
+const babelLoaderConfig = {
+  loader: "babel-loader",
+  options: {
+    plugins: [
+      "@babel/plugin-syntax-dynamic-import",
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread",
+    ],
+    presets: ["@babel/typescript"],
+  },
+};
+
+const baseConfig = {
+  entry: [path.join(process.cwd(), "src/index.ts")],
+  mode: "production",
+  devtool: "eval-source-map",
+  resolve: {
+    alias: { ...aliases },
+    extensions: [".js", ".mjs", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        include: path.join(process.cwd(), "src"),
+        use: [babelLoaderConfig],
+      },
+      {
+        test: /\.m?js$/,
+        include: path.join(process.cwd(), "src"),
+        use: [babelLoaderConfig],
+      },
+    ],
+  },
+};
+
+const umdConfig = {
+  ...baseConfig,
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(process.cwd(), "dist"),
+    library: "svg-to-excalidraw",
+    libraryTarget: "umd",
+    publicPath: "/static/",
+    umdNamedDefine: true,
+  },
+};
+
+const esmConfig = {
+  ...baseConfig,
+  output: {
+    filename: "esm-bundle.js",
+    path: path.resolve(process.cwd(), "dist"),
+    libraryTarget: "module",
+    publicPath: "/static/",
+    umdNamedDefine: true,
+    module: true,
+  },
+  experiments: {
+    outputModule: true,
+  },
+};
+
+module.exports = [umdConfig, esmConfig];
