4bcc7ef6ed263d8577d1af647f793d434044596d
Nicolas Goudry <goudry.nicolas@gmail.com>
2021-02-27 13:10:38 +0100

    feat(path-to-points): enhance commands regex

--- src/elements/path/utils/path-to-points.ts
+++ src/elements/path/utils/path-to-points.ts
@@ -2,7 +2,7 @@ import { Coordinates } from "../../../types";
 import { safeNumber } from "../../../utils";
 import { curveToPoints } from "./bezier";
 
-const PATH_COMMANDS_REGEX = /(?:([HhVv]-?\d+(?:\.\d+)?)|([MmLlTt](?:-?\d+(?:\.\d+)?(?:,| )?){2})|([Ss](?:-?\d+(?:\.\d+)?(?:,| )?){4})|([Cc](?:-?\d+(?:\.\d+)?(?:\.\d+)?(?:,| )?){6})|([Qq](?:-?\d+(?:\.\d+)?(?:\.\d+)?(?:,| )?){4})|(z|Z))/g;
+const PATH_COMMANDS_REGEX = /(?:([HhVv] *-?\d+(?:\.\d+)?)|([MmLlTt](?: *-?\d+(?:\.\d+)?(?:,| *)?){2})|([Cc](?: *-?\d+(?:\.\d+)?(?:\.\d+)?(?:,| *)?){6})|([QqSs](?: *-?\d+(?:\.\d+)?(?:\.\d+)?(?:,| *)?){4})|(z|Z))/g;
 const COMMAND_REGEX = /(?:[MmLlHhVvCcSsQqTtZz]|(-?\d+(?:\.\d+)?))/g;
 
 /**
